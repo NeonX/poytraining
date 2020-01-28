b5c5aa48cbf50ca8b845e02e4208318d577d6444
Poy <Poy@abc.com>
2020-01-28 11:39:50 +0700

    Start App

---
+++ lib/screens/authen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  // Field
+
+  // Method
+  Widget showLogo() {
+    return Container(
+      child: Image.asset('images/logo.png'),
+      width: 125.0,
+    );
+  }
+
+  Widget showAppName() {
+    return Text('poytraining');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            showLogo(),
+            showAppName(),
+          ],
+        ),
+      ),
+    );
+  }
+}
